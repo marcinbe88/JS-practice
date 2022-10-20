e3fd31b4994b2a912af1d976657f87fcaea665d1
Marcin Bejger <marcin.bejger@gmail.com>
2022-10-20 20:39:12 +0200

    challenge-01 DONE

--- 01-algorithms/01-challenge.js
+++ 01-algorithms/01-challenge.js
@@ -10,17 +10,4 @@ function reverseText(str) {
 console.log(reverseText("triathlon"))
 console.log(reverseText(1231232))
 console.log(reverseText(true))
-console.log(reverseText("Marcin"))
-
-
-
-
-
-// const square = function(x) {
-//     return x * x;
-// }
-// console.log(square(6))
-
-// const reverseText = (x) => {
-//     return x * x;
-// }
+console.log(reverseText("M"))
