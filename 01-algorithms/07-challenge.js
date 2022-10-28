83ad99bf33815637e502448b0d9e38dae3a68cfe
Marcin Bejger <marcin.bejger@gmail.com>
2022-10-28 17:52:40 +0200

    challenge-07 DONE

---
+++ 01-algorithms/07-challenge.js
@@ -0,0 +1,17 @@
+function tagFinder(string) {
+    let result = []
+    const stringArray = string.split(" ")
+
+    for (let i = 0; i < stringArray.length; i++) {
+        if (stringArray[i][0] === '#') {
+            const hashtagArray = stringArray[i].split("")
+            const hashtagWord = hashtagArray.slice(1).join("")
+            result.push(hashtagWord)
+        }
+    }
+    return result
+}
+
+console.log(tagFinder('Lubie #plywanie'))
+console.log(tagFinder('#Gdynia uwielbia #triathlon'))
+console.log(tagFinder('Jestem w trakcie nauki #js #javascript'))
