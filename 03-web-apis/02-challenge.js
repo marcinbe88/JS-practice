d7075328db731ebc6efd0ed436193c2397755374
Marcin Bejger <marcin.bejger@gmail.com>
2022-11-15 19:42:29 +0100

    challenge-02 started

---
+++ 03-web-apis/02-challenge.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('toast-button')
+
+function displayToastMessage() {
+    
+    console.log('dziala')
+}
+
+button.addEventListener('click', displayToastMessage)
