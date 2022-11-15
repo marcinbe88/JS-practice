d1d942620a648b5cdca6914c9d1afc64bce0569f
Marcin Bejger <marcin.bejger@gmail.com>
2022-11-15 21:47:18 +0100

    challenge-02 enough for today

--- 03-web-apis/02-challenge.js
+++ 03-web-apis/02-challenge.js
@@ -1,8 +1,7 @@
-function displayToastMessage(text) {
+function displayToastMessage() {
     const el = document.getElementById("element")
-    el.firstChild.nodeValue = text
     console.log('dziala')
 }
 
 const toastButton = document.getElementById("toast-button")
-toastButton.addEventListener('click', () => {displayToastMessage("dupa")})
+toastButton.addEventListener('click', displayToastMessage)
