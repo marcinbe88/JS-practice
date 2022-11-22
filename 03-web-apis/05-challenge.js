56110f6da716f22cf6cc2e31f37f1950181df549
Marcin Bejger <marcin.bejger@gmail.com>
2022-11-22 16:37:13 +0100

    challenge-05 fetch data from api ready

--- 03-web-apis/05-challenge.js
+++ 03-web-apis/05-challenge.js
@@ -2,4 +2,25 @@ fetch("https://jsonplaceholder.typicode.com/users")
   .then((res) => res.json())
   .then((users) => {
     console.log(users)
+    console.log(users.length)
+    function createUserCard() {
+      for (let i = 0; i < users.length; i++) {
+      const newUserCard = document.createElement("div")
+      const newContent = document.createTextNode(`
+      ${users[i].name}
+      ${users[i].username}
+      ${users[i].email}
+      ${users[i].address.city}
+      `)
+      newUserCard.appendChild(newContent)
+    
+      const currentDiv = document.getElementById("user-card")
+      document.body.insertBefore(newUserCard, currentDiv)
+      }
+    }
+    
+    createUserCard()
 })
+
+
+
