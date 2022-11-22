ee26cdc7e551f8e4516be0e36602a082b2d9c319
Marcin Bejger <marcin.bejger@gmail.com>
2022-11-22 19:41:35 +0100

    challenge-05 DONE

--- 03-web-apis/05-challenge.js
+++ 03-web-apis/05-challenge.js
@@ -1,26 +1,40 @@
 fetch("https://jsonplaceholder.typicode.com/users")
   .then((res) => res.json())
   .then((users) => {
-    console.log(users)
-    console.log(users.length)
+
+    const usersList = document.getElementById("users-list")
+
     function createUserCard() {
+      
       for (let i = 0; i < users.length; i++) {
-      const newUserCard = document.createElement("div")
-      const newContent = document.createTextNode(`
-      ${users[i].name}
-      ${users[i].username}
-      ${users[i].email}
-      ${users[i].address.city}
-      `)
-      newUserCard.appendChild(newContent)
-    
-      const currentDiv = document.getElementById("user-card")
-      document.body.insertBefore(newUserCard, currentDiv)
+        const userAll = document.createElement("div")
+        const userPicture = document.createElement("div")
+        const userInfo = document.createElement("div")
+        const nameElement = document.createElement("h2")
+        const usernameElement = document.createElement("h3")
+        const emailElement = document.createElement("p")
+        const cityElement = document.createElement("p")
+        const moreInfo = document.createElement("a")
+
+        userPicture.innerHTML = `<img src=https://picsum.photos/80/120?random=${users[i].id}>`;
+        nameElement.innerText = users[i].name
+        usernameElement.innerText = users[i].username
+        emailElement.innerText = users[i].email
+        cityElement.innerText = users[i].address.city
+        moreInfo.innerText = 'More info...'
+        moreInfo.href = "#"
+
+        userInfo.append(
+          nameElement,
+          usernameElement,
+          emailElement,
+          cityElement,
+          moreInfo
+        )
+
+        userAll.append(userPicture, userInfo)
+        usersList.append(userAll)
       }
     }
-    
     createUserCard()
 })
-
-
-
