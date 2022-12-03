99af1483eea3ab395769e6620c9f5517be10a1ad
Marcin Bejger <marcin.bejger@gmail.com>
2022-12-03 20:45:14 +0100

    adding new notes feature done

--- 04-project/02-challenge.js
+++ 04-project/02-challenge.js
@@ -1,6 +1,16 @@
 const addButton = document.getElementById("add-note-btn")
+const notesSection = document.getElementById("notes-section")
+
 addButton.addEventListener('click', createNewNote)
 
 function createNewNote() {
     console.log('new note!')
+    const newNote = document.createElement("div")
+    newNote.innerHTML = `
+    <div id='note-box'>
+    <input type='text' id='input-section'></input>
+    </div>
+    `
+    notesSection.append(newNote)
+
 }
