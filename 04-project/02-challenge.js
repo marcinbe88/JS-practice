ea0fec387d63ad933b4c17c10934252f56698a91
Marcin Bejger <marcin.bejger@gmail.com>
2022-12-04 13:14:41 +0100

    note box view updated, function deleteNote started

--- 04-project/02-challenge.js
+++ 04-project/02-challenge.js
@@ -1,16 +1,23 @@
 const addButton = document.getElementById("add-note-btn")
 const notesSection = document.getElementById("notes-section")
+const deleteButton = document.getElementById("delete-btn")
 
 addButton.addEventListener('click', createNewNote)
+deleteButton.addEventListener('click', deleteNote)
 
 function createNewNote() {
     console.log('new note!')
     const newNote = document.createElement("div")
     newNote.innerHTML = `
     <div id='note-box'>
-    <input type='text' id='input-section'></input>
+        <button id="delete-btn">Delete</button>
+        <input type='text' id='input-section'></input>
     </div>
     `
     notesSection.append(newNote)
+}
 
+function deleteNote() {
+    console.log('delete note!')
+    delete notesSection
 }
